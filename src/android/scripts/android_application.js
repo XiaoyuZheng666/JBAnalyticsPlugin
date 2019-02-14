837eb6ced2aba0344b0caca00cdb0364daa75d72
xiaoyuzheng <xiaoyuzheng@huatongsilver.com>
2019-02-14 16:02:48 +0800

    完善

--- src/android/scripts/android_application.js
+++ src/android/scripts/android_application.js
@@ -10,7 +10,12 @@ module.exports = function(context) {
 
   var manifestFile = path.join(platformRoot, 'AndroidManifest.xml');
 
+    console.log(manifestFile)
+
   if (fs.existsSync(manifestFile)) {
+
+	    console.log("进入了manifestFile")
+
 
     fs.readFile(manifestFile, 'utf8', function (err,data) {
       if (err) {
